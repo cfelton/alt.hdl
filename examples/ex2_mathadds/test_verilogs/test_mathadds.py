87d0a6528216cfa6ba9c11ba6b7ab0db6f1b4ea5
chiggs <chris.higgs@potentialventures.com>
2014-06-23 09:33:37 +0100

    Ensure "vcd" directory exists - Icarus doesn't create one

--- examples/ex2_mathadds/test_verilogs/test_mathadds.py
+++ examples/ex2_mathadds/test_verilogs/test_mathadds.py
@@ -36,6 +36,10 @@ def _prep_cosim(args, **sigs):
     # get the handle to the
     print("cosimulation setup ...")
     cmd = "vvp -m ./myhdl.vpi mathadds"
+
+    if not os.path.exists("vcd"):
+        os.makedirs("vcd")
+
     return Cosimulation(cmd, **sigs)
 
 
