6e1a3cf68e9938bff899cdb8aadb503efa30a36a
Christopher Felton <chris.felton@gmail.com>
2015-03-06 10:26:22 -0600

    Update intall_myhdl.sh

--- scripts/ci/intall_myhdl.sh
+++ scripts/ci/intall_myhdl.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 git clone https://github.com/jandecaluwe/myhdl
-python myhdl/setup.py install
-make -C myhdl/cosimulation/icarus
+cd myhdl
+python setup.py install
+make -C cosimulation/icarus
 # copy the VPI to the test directory
-cp myhdl/cosimulation/icarus/myhdl.vpi ../../examples/ex1_wprcnt/test_verilogs
-cp myhdl/cosimulation/icarus/myhdl.vpi ../../examples/ex2_mathadds/test_verilogs
-cp myhdl/cosimulation/icarus/myhdl.vpi ../../examples/ex3_zpexgcd/test_verilogs
-cp myhdl/cosimulation/icarus/myhdl.vpi ../../examples/ex4_mathsop/test_verilogs
+cp cosimulation/icarus/myhdl.vpi ../examples/ex1_wprcnt/test_verilogs
+cp cosimulation/icarus/myhdl.vpi ../examples/ex2_mathadds/test_verilogs
+cp cosimulation/icarus/myhdl.vpi ../examples/ex3_zpexgcd/test_verilogs
+cp cosimulation/icarus/myhdl.vpi ../examples/ex4_mathsop/test_verilogs
