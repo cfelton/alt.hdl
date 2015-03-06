cb8d3309be1e6446ef3defea435e915ad51ae670
Christopher Felton <chris.felton@gmail.com>
2015-03-06 17:13:23 -0600

    figure save only, don't pause for image window.

--- examples/ex4_mathsop/test_verilogs/test_mathsop.py
+++ examples/ex4_mathsop/test_verilogs/test_mathsop.py
@@ -159,7 +159,6 @@ def test_mathsop_verilogs(args):
 
     for ext in ('png','pdf',):
         fig.savefig("plots/mathsop_time_response.%s"%(ext))
-    raw_input("continue")
 
 
 if __name__ == '__main__':
